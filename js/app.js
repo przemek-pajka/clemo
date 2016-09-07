cc330f158b1574d95662be85e344cad30598a46f
Paccio <sonys222@gmail.com>
2016-09-07 13:15:32 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function()
+{
+
+
+$(document).on('scroll',function()
+{
+  var lic = $(document).scrollTop();
+
+console.log(lic);
+if(lic>550){
+$(".feature-1 .wp-1").addClass("animated fadeInRight");
+$(".feature-1 .wp-2").addClass("animated fadeInLeft");	};
+
+if(lic>1100){
+$(".feature-2 .wp-3").addClass("animated fadeInRight");
+$(".feature-2 .wp-4").addClass("animated fadeInLeft");	};
+
+if(lic>1650){
+$(".feature-3 .wp-5").addClass("animated fadeInRight");
+$(".feature-3 .wp-6").addClass("animated fadeInLeft");	};	
+
+});
+
+
+});
